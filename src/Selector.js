18c93948d03c820f7c82e7d82bdfbb9e3c1a72bf
Mario <mario.perez094@gmail.com>
2023-07-13 11:45:50 -0700

    rewrote some of the code

--- src/Selector.js
+++ src/Selector.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 class Selector extends React.Component {
   render() {
-    const { options, value, onSelect } = this.props;
+    const { options, value, onSelect, loading } = this.props;
 
     return (
-      <select value={value} onChange={(e) => onSelect(e)} className='text-center shadow p-3 mb-5 bg-body rounded'>
+      <select value={value} onChange={(e) => onSelect(e)} className='text-center shadow p-3 mb-5 bg-body rounded' disabled={ loading }>
         {options.length > 0 ? options.map((option, index) => {
           return <option key={index}
-            value={option.name}>
-            {option.name}
+            value={option.acronym}>
+            {option.acronym}
           </option>
 
         }) : <option>No options</option>}
