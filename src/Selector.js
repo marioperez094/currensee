cae6a7cb438f34c777764666c77a2d7f35cd0f8e
Mario <mario.perez094@gmail.com>
2023-07-13 08:47:36 -0700

    reventing to commit cc5dc7c to delete gh-pages files

--- src/Selector.js
+++ src/Selector.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 class Selector extends React.Component {
   render() {
-    const { options, value, onSelect, label } = this.props;
+    const { options, value, onSelect } = this.props;
 
     return (
-      <React.Fragment>
       <select value={value} onChange={(e) => onSelect(e)} className='text-center shadow p-3 mb-5 bg-body rounded'>
         {options.length > 0 ? options.map((option, index) => {
           return <option key={index}
@@ -15,9 +14,6 @@ class Selector extends React.Component {
 
         }) : <option>No options</option>}
       </select>
-
-      <label>{label}</label>
-      </React.Fragment> 
     )
   }
 }
